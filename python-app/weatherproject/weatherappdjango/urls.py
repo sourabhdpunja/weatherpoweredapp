1167d6ee4df7d1a1158195a229ead9fe4bd8bbab
sourabhdpunja <punja.so@husky.neu.edu>
2019-03-04 16:06:38 -0500

    deleted pyton files

--- python-app/weatherproject/weatherappdjango/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from weatherappdjango.views import postCredentials
-
-urlpatterns = [
-    url(r'postcredentials/', postCredentials, name='postCredentials'),
-    # url(r'getCredentials/', views.index, name='index')
-];
